beca9babb024c5f91facb9e6e73e5d5536b000d2
Mate Radnai <mate_radnai@epam.com>
2022-11-17 19:45:17 +0100

    plus series catalan number and factorial solution

--- plus/series/catalanNumber.js
+++ plus/series/catalanNumber.js
@@ -3,6 +3,41 @@
  * Catalan number sequence.
  * https://en.wikipedia.org/wiki/Catalan_number
  * 
+ * cn = 2n!/(n+1)!*n!
+ * 
  * @param {number} n (n >= 0)
  * @returns {number}
  */
+
+function catalanNumber(n) {
+    if (n < 0 || typeof n !== 'number') {
+        throw new TypeError("Type mismatch!");
+    } else if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        // let sum = 0;
+        // for (let i = 0; i < n; i++) {
+        //     sum += catalanNumber(i) * catalanNumber(n - i -1)
+        //     console.log("SUM: ", sum);
+        // }
+        // return sum;
+         return factorial(2 * n) / (factorial(n + 1) * factorial(n));
+    }
+
+}
+
+function factorial(n) {
+    if (n < 0) {
+        throw new Error("The number cannot be smaller than zero!")
+    } else if (n === 0) {
+        return 1;
+    } else {
+        for (let i = n - 1; i >= 1; i--) {
+            n *= i;
+        }
+        return n;
+        // return (n * factorial(n - 1));
+    }
+}
+
+module.exports = catalanNumber;
--- plus/series/factorial.js
+++ plus/series/factorial.js
@@ -6,3 +6,19 @@
  * @param {number} n (n >= 0)
  * @returns {number}
  */
+
+function factorial(n) {
+    if (n < 0 || typeof n !== 'number') {
+        throw new TypeError("The number cannot be smaller than zero or type mismatch!")
+    } else if (n === 0) {
+        return 1;
+    } else {
+        for (let i = n - 1; i >= 1; i--) {
+            n *= i;
+        }
+        return n;
+        // return (n * factorial(n - 1));
+    }
+}
+
+module.exports = factorial;
