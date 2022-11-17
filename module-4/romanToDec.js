239b3e706f63eef05dcc5609bc1db688ee585c06
Mate Radnai <mate_radnai@epam.com>
2022-11-17 21:24:26 +0100

    solution of romanToDec of module-4

--- module-4/romanToDec.js
+++ module-4/romanToDec.js
@@ -1,3 +1,14 @@
+const restrictedCharRegex = /[^ivxlcdm]/;
+const romanNumbers = {
+    i: 1,
+    v: 5,
+    x: 10,
+    l: 50,
+    c: 100,
+    d: 500,
+    m: 1000
+}
+
 /**
  * It receives a Roman number (as string)
  * and converts it to its Arabic (decimal) equivalent.
@@ -7,3 +18,32 @@
  * @returns {number} the Arabic (decimal) equivalent of the parameter
  * @throws Error in case of invalid input
  */
+
+function romanToDec(roman) {
+    if (typeof roman !== "string") {
+        console.warn('Not a string');
+    } else if (restrictedCharRegex.test(roman) === true) {
+        console.warn('Not a valid roman number');
+        return false;
+    } else {
+        let sum = 0;
+        let currentValue;
+        let nextValue;
+
+        for (let i = 0; i <= roman.length; i++) {
+            currentValue = romanNumbers[`${roman.charAt(i)}`];
+            nextValue = romanNumbers[`${roman.charAt(i + 1)}`];
+
+            if (currentValue >= nextValue) {
+                sum += currentValue;
+            } else if (currentValue < nextValue) {
+                sum -= currentValue;
+            } else if (currentValue && !nextValue) {
+                sum += currentValue;
+            }
+        }
+        return sum;
+    }
+}
+
+module.exports = romanToDec;
